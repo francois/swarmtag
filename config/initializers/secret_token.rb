08fa9123a9b9c5973036639b88daa700bfd5e75c
Marc-Andre Gagnon <magagnon@praizedmedia.com>
2011-07-11 15:12:52 -0400

    Bootstrap for rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swarmtag::Application.config.secret_token = 'ff53a1ed4d9ceb3d8a2502f0b2a5500804a0e01eb0eaef14ecdf25baa1adc18dc71b46ce52b9187850e8857b098cd4cdffc97b0a0ec4a1aec49dca21549d4343'
