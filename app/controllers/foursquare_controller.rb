ad786b97f906d02933ebf0aad277fe9644f1249a
Marc-Andre Gagnon <magagnon@praizedmedia.com>
2011-07-11 16:49:31 -0400

    Added error handling for missing ll

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,7 +1,12 @@
 class FoursquareController < ApplicationController
 
   def trending
-    @venues = Skittles.trending(params[:ll], :limit => 10)
+    begin
+      @venues = Skittles.trending(params[:ll], :limit => 10)
+    rescue Skittles::Error => e
+      @venues = []
+    end
+
     render :json => @venues
   end
 
